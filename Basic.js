d001f7e3ffe79c575cda8e934e158ef714027872
ownupalways <oluwadipegodwin@gmail.com>
2021-11-15 04:53:47 -0800

    School application
    This is a school application built for easy access for both new and existing students.

---
+++ Basic.js
@@ -0,0 +1,9 @@
+function myFunction(){
+	var x =document.getElementById("myTopnav");
+
+	if (x.className === "menuarea"){
+		x.className += " responsive";
+	}else{ 
+		x.className = "menuarea";
+	}
+}
